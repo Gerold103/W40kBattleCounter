1558226736f714cb456329e09eac8bcf49aa5091
aemaet <darkinard@gmail.com>
2014-02-10 00:52:50 +0400

    Another minor changes to interface.

--- CombatLib/CombatLib/AdditionalFuncs.cs
+++ CombatLib/CombatLib/AdditionalFuncs.cs
@@ -13,7 +13,9 @@ namespace CombatLib.Addition
             int prevElem = 0;
             foreach (int elem in arg)
             {
-                if (arg[0] == 0 || arg[arg.Length - 1] == 0 || prevElem + elem == 0) throw new ApplicationException("Wrong Input");
+                if (arg[0] == 0) throw new ApplicationException("Wrong Input: first element = 0");
+                if (arg[arg.Length - 1] == 0) throw new ApplicationException("Wrong Input: last element = 0");
+                if (prevElem + elem == 0) throw new ApplicationException("Wrong Input: repeating 0's");
                 prevElem = elem;
             }
         }
@@ -23,12 +25,12 @@ namespace CombatLib.Addition
             String res = "";
             foreach (int elem in arg)
             {
-                if (elem != 0) res += (char)(elem + '0');
+                if (elem == 0) res = res.Substring(0,res.Length-1) + '→';
                 else
                 {
-                    res += '→';
+                    res += (char)(elem + '0');
+                    res += ' ';
                 }
-                res += ' ';
             }
             return res;
         }
