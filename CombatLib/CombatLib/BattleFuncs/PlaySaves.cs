78e9c4ef32a9b883471533c99c75397c1e0c5eeb
Gerold103 <yaaaaashik@mail.ru>
2014-02-11 23:18:47 +0400

    Написано тело функции для фазы спасов для пехоты.

--- CombatLib/CombatLib/BattleFuncs/PlaySaves.cs
+++ CombatLib/CombatLib/BattleFuncs/PlaySaves.cs
@@ -11,28 +11,23 @@ namespace CombatLib.BattleFuncs
 
     public static class PlaySaves
     {
-        /// <summary>
-        /// Спасброски за пехоту
-        /// </summary>
-        /// <param name="Wounds"></param>
-        /// <param name="Saves"></param>
-        /// <param name="RndGenerator"></param>
-        /// <returns></returns>
-        public static int Play(CombatLib.Phases.PhaseWounds.PhaseWoundsInfantry Wounds, ref CombatLib.Phases.PhaseSaves.PhaseSavesInfantry Saves, ref Random RndGenerator) //Спасброски за пехоту, начальные данные
+        public static int Play(ref CombatLib.Phases.PhaseWounds.PhaseWoundsInfantry Wounds, ref CombatLib.Phases.PhaseSaves.PhaseSavesInfantry Saves, ref Random RndGenerator) //Спасброски за пехоту, начальные данные
         //берутся из PhaseWoundsInfantry, результат записывается в PhaseSavesInfantry, случайные числа получаются из экземпляра класса Random, возвращает количество спасенных
         {
-            Console.WriteLine("PlaySaves, aim - infantry");
+            Console.WriteLine("PlaySaves, aim - infantry"); //отладочный вывод
+            int i; //счетчик всех попыток спасения
+            Saves.SaveCubes = new int[Wounds.RowWounds]; //выделение памяти под массив кубов на спасброски. Количество бросков = количеству потенциальных ран
+
+            for (i = 0; i < Wounds.RowWounds; i++)
+            {
+                Saves.SaveCubes[i] = RndGenerator.Next(1, 7);
+                if (Saves.SaveCubes[i] >= Saves.Condition) Saves.Saves++;
+            }
+            Wounds.Wounds = Wounds.RowWounds - Saves.Saves; //Итоговое количество ран = разница между потенциальными и спасшимися
             return 0;
         }
 
-        /// <summary>
-        /// Спасброски за технику
-        /// </summary>
-        /// <param name="Wounds"></param>
-        /// <param name="Saves"></param>
-        /// <param name="RndGenerator"></param>
-        /// <returns></returns>
-        public static int Play(CombatLib.Phases.PhaseWounds.PhaseWoundsVehicle Wounds, ref CombatLib.Phases.PhaseSaves.PhaseSavesVehicle Saves, ref Random RndGenerator) //Спасброски за технику, начальные данные
+        public static int Play(ref CombatLib.Phases.PhaseWounds.PhaseWoundsVehicle Wounds, ref CombatLib.Phases.PhaseSaves.PhaseSavesVehicle Saves, ref Random RndGenerator) //Спасброски за технику, начальные данные
         //берутся из PhaseWoundsVehicle, результат записывается в PhaseSavesVehicle, случайные числа получаются из экземпляра класса Random, возвращает количество спасенных
         {
             Console.WriteLine("PlaySaves, aim - vehicle");
